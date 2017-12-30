1b76fb48ee4f3e491cc03a3bad0d1c8194ee5571
Keita Moromizato <keita.moromi@gmail.com>
2017-12-30 13:16:01 +0900

    markdown preview

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hoge")
+const marked = require("marked");
+
+const memoArea = document.querySelector("#memoArea");
+const previewArea = document.querySelector("#previewArea");
+
+memoArea.addEventListener("input", e => {
+  previewArea.innerHTML = marked(e.target.value);
+});
